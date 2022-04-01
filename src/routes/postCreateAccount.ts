ac54425f89430c5dae706de3d88a1c851e0ec526
Tudor Harries <tharries508@gmail.com>
2022-04-01 16:26:43 +0100

    Extract account out of post request

--- src/routes/postCreateAccount.ts
+++ src/routes/postCreateAccount.ts
@@ -1,11 +1,16 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { Account } from "../types/Account";
 
 export const postCreateAccountRouter = express.Router();
 
-postCreateAccountRouter.get("/account", bodyParser.json(), (req, res) => {
-//   const title = (req.params.title as unknown) as Pick<Lesson, "title">;
-//   getLesson(title).send((err, data) =>
-//     err ? res.send(err) : res.send(data.Item)
-//   );
-});
+postCreateAccountRouter.get(
+  "/account",
+  bodyParser.json(),
+  (req: express.Request<Account>, res) => {
+    const account = req.params;
+    //   getLesson(title).send((err, data) =>
+    //     err ? res.send(err) : res.send(data.Item)
+    //   );
+  }
+);
