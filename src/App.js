ae52803e98622d313e54713deb81919e70764e22
Tudor Harries <tharries508@gmail.com>
2022-05-04 11:31:47 +0100

    Fix naming, move "home" into a component

--- src/App.js
+++ src/App.js
@@ -4,25 +4,29 @@ import Quiz from "./components/Quiz";
 import Quizzes from "./components/Quizzes";
 import SignUp from "./components/SignUp";
 import QuizPlayer from "./components/QuizPlayer";
+import Home from "./components/Home";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      <h1>Home</h1>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
       <div>
         <Question />
         <Quiz />
         <Quizzes />
         <li>
-          <Link to="/login">Login</Link>
+          <Link to="/signup">Signup</Link>
         </li>
         <QuizPlayer />
       </div>
 
       <Routes>
-        <Route path="login" element={<SignUp />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
