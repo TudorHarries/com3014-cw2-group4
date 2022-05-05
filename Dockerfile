28c0d5f2a58ca8af48a5bbb58b584e1de0adacc6
Tudor Harries <tharries508@gmail.com>
2022-05-05 19:42:15 +0100

    Add docker-compose

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ WORKDIR /usr/src/app
 # where available (npm@5+)
 COPY package*.json ./
 
-RUN npm install
+RUN npm install && npm install typescript -g
 # If you are building your code for production
 # RUN npm ci --only=production
 
@@ -17,6 +17,6 @@ COPY . .
 
 EXPOSE 8080
 
-RUN npm run build
+RUN tsc
 
 CMD [ "node", "dist/app.js" ]
--- dist/routes/postLogout.js
+++ dist/routes/postLogout.js
@@ -12,16 +12,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getAccountRouter = void 0;
+exports.postLoginRouter = void 0;
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const db_1 = require("../scripts/db");
+const bcryptjs_1 = require("bcryptjs");
 const dbName = "test";
 const collectionName = "accounts";
-exports.getAccountRouter = express_1.default.Router();
-exports.getAccountRouter.get("/account/:email", body_parser_1.default.json(), (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { email } = req.params;
-    const account = yield (0, db_1.getAccount)(dbName, collectionName, email);
-    res.send(account);
+exports.postLoginRouter = express_1.default.Router();
+exports.postLoginRouter.post("/login", body_parser_1.default.json(), (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const body = req.body;
+    const account = yield (0, db_1.getAccount)(dbName, collectionName, body.email);
+    // We should return more descriptive stuff than just false
+    const result = account != undefined
+        ? yield (0, bcryptjs_1.compare)(body.password, account.password)
+        : false;
+    res.send(result);
 }));
-//# sourceMappingURL=getAccount.js.map
+//# sourceMappingURL=postLogout.js.map
