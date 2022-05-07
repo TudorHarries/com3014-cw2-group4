581c020370869e71a59c1ab3aaf6931a07d1bb49
ShumsZ <siwanishums@gmail.com>
2022-05-07 22:02:01 +0100

    added get question functionality

--- question/src/controller/question.controller.ts
+++ question/src/controller/question.controller.ts
@@ -1,14 +1,27 @@
 import { Request, Response } from 'express';
-import { createQyestionInput } from '../schema/question.schema';
-import { createQuestion } from '../service/question.service';
+import { createQuestionInput } from '../schema/question.schema';
+import { createQuestion, getQuestion } from '../service/question.service';
 import logger from "../utils/logger";
 
-export async function createQuestionHandler(req: Request<{}, {}, createQyestionInput['body']>, res: Response) {
+export async function createQuestionHandler(req: Request<{}, {}, createQuestionInput['body']>, res: Response) {
     try {
         const dbName = "test";
         const collectionName = "questions";
         const question = await createQuestion(dbName, collectionName, req.body);
         return res.send(question);
+    }
+    catch (e: any) {
+        logger.error(e)
+        return res.status(404)
+    }
+}
+
+export async function getQuestionHandler(req: Request, res: Response) {
+    try {
+        const dbName = "test";
+        const collectionName = "questions";
+        const questions = await getQuestion(dbName, collectionName, req.params.question);
+        return res.send(questions);
     }
     catch (e: any) {
         logger.error(e)
--- question/src/routes.ts
+++ question/src/routes.ts
@@ -1,5 +1,5 @@
 import { Express, Request, Response } from "express";
-import { createQuestionHandler } from "./controller/question.controller";
+import { createQuestionHandler, getQuestionHandler } from "./controller/question.controller";
 import validateResource from './middleware/validateResource';
 import { createQuestionSchema } from "./schema/question.schema";
 import bodyParser from "body-parser";
@@ -8,6 +8,8 @@ import bodyParser from "body-parser";
 function routes(app: Express) {
 
     app.get('/healthcheck', (req, res) => res.sendStatus(200));
+
+    app.get("/api/questions/:question", getQuestionHandler);
 
     app.post("/api/questions", bodyParser.json(), validateResource(createQuestionSchema), createQuestionHandler);
 
